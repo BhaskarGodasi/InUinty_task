09a0ef72db15f39b4c9e06964eb4150af2a59a33
bhaskargodasi <bhaskaragodasi@gmail.com>
2025-07-11 10:28:01 +0530

    final Commit

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use('/api/goals', goalRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
